3bad74d18420739705fbf534d290b9dcfc726140
Saturnin Dao <saturnindao@gmail.com>
2020-06-07 10:16:09 +0000

    initial commit

---
+++ database/migrations/2020_06_06_170812_create_articles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            /*  
+            $table->integer('user_id')->unsigned();
+            $table->foreign(’user_id’)  
+                  ->references(’id’)  
+                  ->on(’users’);
+                  */
+            // $table->foreignId('user_id')->constrained('users_table');  
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title')->index();  
+            $table->text('content');  
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
