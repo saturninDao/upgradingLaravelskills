6a254e985ea1992434efaf2e703201af2ceac894
Saturnin Dao <saturnindao@gmail.com>
2020-06-10 14:33:07 +0000

    Accedez aux relations 1...1

--- app/Address.php
+++ app/Address.php
@@ -10,5 +10,32 @@ class Address extends Model
     {  
         // Une adresse appartient à une société  
         return $this->belongsTo(Company::class);  
-    }  
+    }
+
+    /*
+        $address = Address::first();  
+        $company_name = $address->company->name;
+
+        Insérer une relation en base de données
+
+        $company = new Company;  
+        $company->name = ’Narval’;  
+        $company->save();  
+        
+        $address = new Address;  
+        $address->city = ’Nantes’;  
+        $address->country = ’France’;  
+        
+        $company->address()->save($address);
+
+        Mettre à jour une relation
+        $company = Company::find(12);  
+        
+        $address = new Address;  
+        $address->city = ’Nantes’;  
+        $address->country = ’France’;  
+        
+        $company->address()->save($address);
+
+    */
 }
--- app/Company.php
+++ app/Company.php
@@ -10,5 +10,16 @@ class Company extends Model
     {  
         // Une société possède une adresse  
         return $this->hasOne(Address::class);  
-    }  
+    } 
+    
+    /*
+        $my_company = Company::where(’name’, ’Narval’)->first();  
+  
+        $my_company_address = $my_company->address;  
+  
+        // Ensuite, on peut manipuler l’adresse  
+        $city = $my_company_address->city;  
+        $my_company_address->country = ’France’;  
+        $my_company_address->save();
+    */
 }
