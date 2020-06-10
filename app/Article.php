bccd217269d6a6b3186ab82fc22c0b5959d1fed8
Saturnin Dao <saturnindao@gmail.com>
2020-06-10 14:35:36 +0000

    Acceder aux relation 1...n

--- app/Article.php
+++ app/Article.php
@@ -17,5 +17,37 @@ class Article extends Model
     {  
         // Un article appartient à une ou plusieurs catégories. 
         return $this->belongsToMany(Category::class);  
+
+        /*
+
+            $article = Article::first();  
+            
+            $comments = $article->comments()  
+                ->where(’text’, ’LIKE’, ’Hello%’)  
+                ->get();
+            Pour enregistrer un commentaire pour un article, deux méthodes sont disponibles : save et saveMany.
+
+            Enregistrement de commentaires
+
+
+            $article = Article::first();  
+            
+            $comment1 = Comment::new();  
+            $comment1->text = ’Parfait’;  
+            
+            $article->comments()->save($comment1);  
+            
+            $comment2 = Comment::new();  
+            $comment2->text = ’Moyen’;  
+            
+            $comment3 = Comment::new();  
+            $comment3->text = ’Mauvais’;  
+            
+            $article->comments()->saveMany([  
+                $comment2,  
+                $comment3,  
+            ]);
+
+        */
     } 
 }
--- app/Comment.php
+++ app/Comment.php
@@ -11,4 +11,26 @@ class Comment extends Model
         // L’article qui contient un commentaire  
         return $this->belongsTo(Article::class);  
     } 
+
+    /*
+    Accéder aux commentaires d’un article
+
+
+        $article = Article::find(4577);  
+        
+        $comments = $articles->comments;  
+        
+        foreach ($comments as $comment) {  
+            echo $comment->title;  
+            // ...  
+            echo $comment->text;  
+        }
+        Accéder à l’article auquel se rapporte un commentaire
+
+
+        $comment = Comment::first();  
+        
+        echo $comment->article->title;
+
+    */
 }
