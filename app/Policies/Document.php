786ed20af0b446b82eb21b3e92a207c5c8ef6416
Saturnin Dao <saturnindao@gmail.com>
2020-06-22 07:54:24 +0000

    j'adapte le policy a notre nouvelle exemple concernant les users et leurs roles

--- app/Policies/Document.php
+++ app/Policies/Document.php
@@ -18,7 +18,7 @@ class Document
     {
         //
     }
-
+/*
     public function show(User $user)  
     {  
         // Tout le monde peut voir un document.  
@@ -37,5 +37,39 @@ class Document
         // Un utilisateur valide a le droit  
         // de créer un document.  
         return $user->is_valid;  
+    } 
+    
+*/
+
+    public function create(User $user)  
+    {  
+        // Un rédacteur à le droit  
+        // de créer un document.  
+        return $user->role == ’writer’;  
+    }  
+
+    public function delete(User $user)  
+    {  
+        // Un modérateur peut  
+        // supprimer un document  
+        return $user->role == ’moderator’;  
+    }  
+
+    public function showAll(User $user)  
+    {  
+        // Un administrateur peut  
+        // afficher tous les documents  
+        return $user->role == ’admin’;  
+    }  
+
+    public function before(User $user)  
+    {  
+        // La méthode before permet d’effectuer  
+        // une vérification avant de passer à la  
+        // suite. Par exemple dans ce cas, si  
+        // l’utilisateur est admin, il est inutile  
+        // de faire d’autres vérifications. Il a  
+        // tous les droits  
+        return $user->role == ’admin’ ?: null;  
     }  
 }
---
+++ database/migrations/2020_06_22_074117_add_role_column.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRoleColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()  
+    {  
+        Schema::table('users', function (Blueprint $table) {  
+            $table->enum(  
+                'role',  
+                ['admin', 'moderator', 'writer', 'default']  
+            )->default('default')->index(); 
+        });  
+    } 
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()  
+    {  
+        Schema::table('users', function (Blueprint $table) {  
+            $table->dropColumn('role');  
+        });  
+    } 
+}
